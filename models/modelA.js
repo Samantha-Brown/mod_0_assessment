6212f8e5cce3ba1b330edaf0c36f2f0393c4f314
Samantha Brown <bro7445@gmail.com>
2021-04-23 18:39:24 -0400

    Completed model A

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+Model an AIRPORT
+
+var denverInternational = {
+  stateLocation: "Colorado",
+  cityLocation: "Denver",
+  employees: 205,
+  bathrooms: 16,
+  closedHolidays: false,
+  isBusy: true,
+  airlineCompanies: ["Delta", "American", "Spirit"],
+  resturants: ["Cinnabon", "McDonalds", "TGIF"]
+}
