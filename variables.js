a3d8863c963cb4a0ed5f04d0ddc30dc078d193ab
Samantha Brown <bro7445@gmail.com>
2021-04-23 18:29:00 -0400

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,16 @@
+var famousPerson = "Oprah"
+var isActive = true
+
+famousPerson = "Cher"
+// alternatively you could do Oprah = "Cher"
+
+var num3 = 8
+var num9 = 2
+var sum = (num3 + num9);
+
+var animals = ["Tigers", "Elephants", "Pigs"]
+animals.shift();
+//checked with console.log(animals);
+
+const str = "Cher";
+console.log(`${str} ${str.length}`);
